3eb39308c56e88e1c9467a286c68eaae10e3f123
AKclown <13413629530@139.com>
2024-11-27 19:13:18 +0800

    feat: 添加button样式

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <el-icon color="#999">Icon</el-icon>
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
   </div>
 </template>
 <script setup lang="ts"></script>
